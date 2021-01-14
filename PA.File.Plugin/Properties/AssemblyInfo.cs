7662af50d2b2825df57d6013bb9e41648589568a
Thomas <tomgrv@users.perspikapps.fr>
2021-01-14 20:57:47 +0100

    fix: add missing AssemblyInfo.cs files

---
+++ PA.File.Plugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("PA.File.Plugin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Perspikapps")]
+[assembly: AssemblyProduct("PA.File")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// La définition de ComVisible sur False rend les types dans cet assembly invisibles
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir
+// de COM, définissez l'attribut ComVisible sur True pour ce type.
+
+[assembly: ComVisible(false)]
+
+// Le GUID suivant concerne l'ID de typelib si ce projet est exposé à COM.
+
+[assembly: Guid("120f8541-368a-4664-a91e-2d7c7db72ac4")]
---
+++ PA.File/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("PA.File")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Perspikapps")]
+[assembly: AssemblyProduct("PA.File")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur False à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur True à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("6b525ecc-f7a8-41a7-8956-d060deb8217e")]
